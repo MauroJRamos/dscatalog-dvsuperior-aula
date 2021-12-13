89d980fd522b39058aacf1033cc148b3bb585569
MauroJRamos <mauroramosti@gmail.com>
2021-12-13 16:47:24 -0300

    Form validation

--- frontendweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontendweb/src/pages/Admin/Auth/Login/index.tsx
@@ -6,57 +6,70 @@ import { useState } from 'react';
 
 import './styles.css';
 
-
 type FormData = {
   username: string;
   password: string;
 };
 
 const Login = () => {
-
   const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
-    .then(response =>{
-      setHasError(false);
-      console.log('Sucesso', response);
-    })
-    .catch(error =>{
-      setHasError(true);
-      console.log('Erro', error);
-    });
-    
+      .then((response) => {
+        setHasError(false);
+        console.log('Sucesso', response);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log('Erro', error);
+      });
   };
 
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      { hasError &&
-      <div className="alert alert-danger">
-         Erro ao tentar efetuar o Login
-      </div>
-      }
-        <form onSubmit={handleSubmit(onSubmit)}>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o Login</div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
-            {...register('username')}
+            {...register('username', {
+              required: 'campo obrigatório', //Essa mensagem sera impressa por error.username.message
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email valido',
+              }
+            })}
             type="text"
             className="form-control base-input"
             placeholder="Email"
             name="username"
           />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-2">
           <input
-            {...register('password')}
+            {...register('password', {
+              required: 'campo obrigatório', //Essa mensagem sera impressa por error.username.message
+            })}
             type="password"
             className="form-control base-input "
             placeholder="Password"
             name="password"
           />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
